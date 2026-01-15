eb0f6d7fb21558dcff123b91572c7012cbc536a2
nuel071003 <151902942+nuel071003@users.noreply.github.com>
2026-01-15 20:56:29 +0900

    Add files via upload

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBuBKLljnocEwtYbigQZ71uNEHzefiGLqo",
+    authDomain: "kampuseval.firebaseapp.com",
+    projectId: "kampuseval",
+    storageBucket: "kampuseval.firebasestorage.app",
+    messagingSenderId: "961805706158",
+    appId: "1:961805706158:web:f8068456bcd60d298e5e3f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
